0022246179503cd59043fda449bd97c1a3a6eac7
Tyler Hostetler <tyhostetler1@gmail.com>
2024-08-04 20:17:51 -0400

    Fixed find_similarity to generate BGC similarity regardless of secondary input

--- find_similarity.py
+++ find_similarity.py
@@ -3,24 +3,26 @@ import SqlRequests as sql
 import pandas as pd
 
 
-def output_table(sql_path, pfam_list):
-    pep_accessions = get_accessions_from_single_list(sql_path, [])
+def output_table(sql_path, pfam_list, secondary_input):
+    pep_accessions = get_accessions_from_single_list(sql_path, [], secondary_input)[0]
     match_list = {}
     for pfam in pfam_list:
-        acc_match = get_accessions_from_single_list(sql_path, [pfam])
+        acc_match = get_accessions_from_single_list(sql_path, [pfam], secondary_input)[0]
         match_list.update({pfam: acc_match})
 
     print(f"PfamList: {pfam_list}\n Pep-Accessions: {pep_accessions}\nMatch_list: {match_list}")
 
-    df = pd.DataFrame(columns=pep_accessions, index=match_list.keys())
-
+    compare_list = []
     for group in match_list:
+        true_false_list = []
         for acc in pep_accessions:
             if acc in match_list[group]:
-                df.at[group, acc] = True
+                true_false_list.append('True')
             else:
-                df.at[group, acc] = False
+                true_false_list.append('False')
+        compare_list.append(true_false_list)
 
+    df = pd.DataFrame(compare_list, columns=pep_accessions, index=match_list.keys())
     transposed_df = df.transpose()
     transposed_df.to_csv('similarity.csv', index=True)
 
@@ -32,9 +34,9 @@ def get_all_combinations(_search_list):
     return combination_list
 
 
-def get_accessions_from_single_list(sql_path, pfam_list):
-    pfam_list.append('PF13714')
-    accessions = sql.get_output(sql_path, pfam_list, 'Accession')
+def get_accessions_from_single_list(sql_path, pfam_list, secondary_input):
+    pfam_list.append(secondary_input)
+    accessions = sql.get_output(sql_path, pfam_list, 'Accession', secondary_input)
     return accessions
 
 
