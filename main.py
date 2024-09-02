e2230df4bec5551a17a7f93ce17978e11816e308
Tyler Hostetler <tyhostetler1@gmail.com>
2024-09-01 21:41:54 -0400

    Add Qt Material Style Theme

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import sys
 import loadMainUI
+from qt_material import apply_stylesheet
 
 if __name__ == '__main__':
     app = loadMainUI.QApplication(sys.argv)
     MainWindow = loadMainUI.QMainWindow()
     UIWindow = loadMainUI.MainUI('ui_main_window.ui')
     UIWindow.window.show()
+
+    # Changing UI Style
+    apply_stylesheet(app, theme='custom_ui_theme.xml')
 
     sys.exit(app.exec())
